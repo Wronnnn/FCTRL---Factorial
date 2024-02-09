7b26cc01fa591ebd3c7fe0c86490a939b4e2d689
Wronnnn <skowronmaciej@protonmail.com>
2024-02-09 20:06:38 +0100

    Jedank nie bo coś zepsułem

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
 int main() {
-    int quantity, number, count;
+    int quantity, count;
     long long int temp;
     std::cin>>quantity;
+    int arr[quantity];
+    for(int i=0;i<quantity;i++){
+        std::cin>>arr[i];
+    }
     for(int i=0;i<quantity;i++){
         temp = 1;
         count = 0;
-        std::cin>>number;
-        for(int j=1;j<=number;j++){
+        for(int j=1;j<=arr[i];j++){
             temp *= j;
             bool enterToCounter = true;
             while(temp>=10){
@@ -20,7 +23,10 @@ int main() {
                 temp = temp / 10;
             }
         }
-        std::cout<<temp;
+        arr[i]=count;
+    }
+    for(int i=0;i<quantity;i++){
+        std::cout<<arr[i]<<std::endl;
     }
     return 0;
 }
